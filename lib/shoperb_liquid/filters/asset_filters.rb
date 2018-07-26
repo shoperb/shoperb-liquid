9b3214af61f6605a871e3e13d88629c788336c5e
Myroslava Stavnycha <myroslava@perfectline.co>
2018-07-26 10:25:28 +0300

    remove some redundant frozen_string_literal

--- lib/shoperb_liquid/filters/asset_filters.rb
+++ lib/shoperb_liquid/filters/asset_filters.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 module ShoperbLiquid
   module AssetFilters
     include ActionView::Helpers::AssetTagHelper
--- lib/shoperb_liquid/filters/html_filters.rb
+++ lib/shoperb_liquid/filters/html_filters.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 module ShoperbLiquid
   module HtmlFilters
     def modulo_word(word, index, modulo)
