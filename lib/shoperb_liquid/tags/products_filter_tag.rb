d9991343bedc30ab091cced66ae63c2e5e1dd23e
Vladimir Krylov <vladimir.krylov@perfectline.co>
2021-01-14 10:34:23 +0200

    product filter tag can return only labels

--- lib/shoperb_liquid/tags/products_filter_tag.rb
+++ lib/shoperb_liquid/tags/products_filter_tag.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 module ShoperbLiquid
-  # Generates faceted/aggregated 
-  #search markup data
+  # Generates faceted/aggregated
+  # search markup data
   #
   # Usage:
   #
@@ -19,11 +19,23 @@ module ShoperbLiquid
   #   {% endfor %}
   # {% products_filter %}
   #
+  # you can also use next variables like `products_filter, what: 'full'`:
+  #   * what - one of ['full', 'labels'], default: 'full'
   class ProductsFilterTag < ::Liquid::Block
-    def initialize(tag_name, markup, tokens)
+    FULL   = 'full'
+    LABELS = 'labels'
+    def initialize(tag_name, markup, options)
+      @parse_context = options # mark for parser
+      @attributes = {}
+      markup.scan(Liquid::TagAttributes) do |key, value|
+        @attributes[key] = parse_expression(value)
+      end
+
+      @attributes["what"] ||= FULL
+
       super
     end
-    
+
     def facets(context)
       json    = {}
       row     = {}
@@ -37,12 +49,35 @@ module ShoperbLiquid
             json[va.handle]["values"][va.value]         ||= { "label" => va.value, "count" => 1 }
             json[va.handle]["values"][va.value]["count"]  = pr_idxs["#{va.handle}:#{va.value}"].size
           end
-          
+
           row[:min] = v.price.to_d if row[:min].nil? || row[:min] > v.price.to_d
           row[:max] = v.price.to_d if row[:max].nil? || row[:max] < v.price.to_d
         end
       end
 
+      set_prices(json, row)
+      json
+    end
+
+    def labels(context)
+      json    = {}
+      row     = {}
+      context["products"].to_a.each_with_index do |pr, product_idx|
+        pr.variants.each do |v|
+          v.attributes.each do |va|
+            json[va.handle] ||= { "name"  => va.name, "values" => {}}
+          end
+
+          row[:min] = v.price.to_d if row[:min].nil? || row[:min] > v.price.to_d
+          row[:max] = v.price.to_d if row[:max].nil? || row[:max] < v.price.to_d
+        end
+      end
+
+      set_prices(json, row)
+      json
+    end
+
+    def set_prices(json, row)
       json["price"] = {
           "name"   => "Price",
           "values" => {
@@ -50,15 +85,18 @@ module ShoperbLiquid
               "max" => {"label"=>"Maximum", "count"=>row[:max]}
           }
       }
-      json
     end
-    
-    
+
+
+    # liquid < 5.0
     def render(context)
       context.stack do
         nodelist = []
-        
-        context["groups"] = facets(context).stringify_keys!
+        context["groups"] = case @attributes["what"]
+        when FULL   then facets(context).stringify_keys!
+        when LABELS then labels(context).stringify_keys!
+        else {}
+        end
         nodelist << super # other part of html
         nodelist.join("<br />")
       end
